e50a9da42b1c34eabd66c463d2981c9a563bb38d
Ivan Q <ivan@tacode.mx>
2020-03-12 13:33:26 -0600

    first setup

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Ivan Quintana on 12/03/20.
+//  Copyright © 2020 Ivan Quintana. All rights reserved.
+//
+
+import UIKit
+
+class PodcastTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
