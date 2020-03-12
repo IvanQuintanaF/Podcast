e53d4e1ebaa4a94595e4131331fbe996ca63e762
Ivan Q <ivan@tacode.mx>
2020-03-12 13:38:03 -0600

    folder config

---
+++ Podcast/Controller/EpisodesTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EpisodesTableViewController.swift
+//  Podcast
+//
+//  Created by Ivan Quintana on 12/03/20.
+//  Copyright © 2020 Ivan Quintana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EpisodesTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Podcast/View/EpisodeCell.swift
@@ -0,0 +1,9 @@
+//
+//  EpisodeCell.swift
+//  Podcast
+//
+//  Created by Ivan Quintana on 12/03/20.
+//  Copyright © 2020 Ivan Quintana. All rights reserved.
+//
+
+import Foundation
